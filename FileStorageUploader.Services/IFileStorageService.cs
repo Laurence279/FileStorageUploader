704391ead53d347e4954f072035e3758d93f995b
Laurence279 <lnunn279@gmail.com>
2024-01-23 02:00:27 +0000

    Add project files

---
+++ FileStorageUploader.Services/IFileStorageService.cs
@@ -0,0 +1,12 @@
+﻿namespace FileStorageUploader.Services
+{
+    using System.IO;
+    using System.Threading.Tasks;
+
+    public interface IFileStorageService
+    {
+        Task<string> UploadAsync(string container, string fileName, Stream stream);
+
+        Task<bool> ExistsAsync(string container, string fileName);
+    }
+}
